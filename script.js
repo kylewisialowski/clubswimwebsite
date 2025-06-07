e95731e666d3dc57e0affe446b15fb8b5961cc1c
kylewisialowski <103215823+kylewisialowski@users.noreply.github.com>
2025-06-07 19:36:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const text = "Welcome to Brown Club Swim";
+const target = document.getElementById("typed-text");
 
+let i = 0;
+function typeCharacter() {
+  if (i < text.length) {
+    target.textContent += text.charAt(i);
+    i++;
+    setTimeout(typeCharacter, 100); // speed of typing
+  }
+}
+
+typeCharacter();
